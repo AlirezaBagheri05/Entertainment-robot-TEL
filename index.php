98977c2eff2670e4de1e574da4456bf19cdd8e26
Alireza <alirezabagheri3461@gmail.com>
2022-08-13 23:40:19 +0430

    wrote some code

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php 
+
+$content = file_get_contents('php://input');
+
+$update = json_decode($content , true);
+
+$chat_id = $update['message']['chat']['id'];
+$text = $update['message']['text'];
+
+$url = "https://api.telegram.org/bot5451704513:AAGHBwi8zE2AjnB9wrIcJzzAgRHN0VI5Hy8/sendMessage?chat_id=$chat_id&text=$text";
+
+file_get_contents($url);
+
+?>
