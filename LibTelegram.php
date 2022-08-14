9b4c875e5182318a06f38d920b0ef3d5837aeaaa
Alireza <alirezabagheri3461@gmail.com>
2022-08-14 13:37:51 +0430

    making libtelegram classes

---
+++ LibTelegram.php
@@ -0,0 +1,34 @@
+<?php
+
+class bot_telegram(){
+    private $API_BOT;
+    
+    public function __construct($BOT){
+        $this->API_BOT = $BOT;
+    }
+
+    private function init($API_BOT){
+        return curl_init($API_BOT);
+    }
+    private function exec($handle){
+        return curl_exec($handle);
+    }
+
+
+    public function sendMessage($method,$parametrs){
+        if(!$parametrs){
+            $parametrs = array();
+        }
+        $parametrs['method'] = $method;
+        $handle = $this->init($API_BOT);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+        curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($parametrs));
+        curl_setopt($handle, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        $result = $this->exec($handle);
+        return $result;
+    }
+
+
+}
--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php 
+require_once('env.php');
+require_once('LibTelegram.php');
 
 $content = file_get_contents('php://input');
-
 $update = json_decode($content , true);
-
 $chat_id = $update['message']['chat']['id'];
 $text = $update['message']['text'];
 
-$url = "https://api.telegram.org/bot5451704513:AAGHBwi8zE2AjnB9wrIcJzzAgRHN0VI5Hy8/sendMessage?chat_id=$chat_id&text=$text";
-
-file_get_contents($url);
+$parametrs = array(
+    'chat_id'=>$chat_id,
+    'text'=>$text
+);
+$firstbot = new bot_telegram(API_URL);
+$result = $fistbot->sendMessage('sendMessage',$parametrs);
+print_r($result);
 
 ?>
